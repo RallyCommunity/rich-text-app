4058f9cd3f358bd8ca8badecede3a09edbc6f718
Kyle Morse <kmorse@rallydev.com>
2018-11-29 14:56:18 -0700

    Actually fix ready event

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -38,7 +38,37 @@ Ext.define("CArABU.app.RichTextApp", {
             margin: '10 70 0 60',
             fieldLabel: 'Informational Text',
             _createResizer: function(){}, //This is an override so that the resizer handle which is hardcoded in the component doesn't hide the last line of the editor.
-            resizeable: false
+            resizeable: false,
+
+            readyEvent: 'afterload',
+            //This function is an override so that the readyEvent
+            //is consistently fired async.  Otherwise we get weird
+            //settings panel missing buttons/sizing issues
+            _drawEditor: function () {
+                var editorLoadPromise = this.createClosureEditor();
+                this._createResizer();
+    
+                editorLoadPromise.always(function () {
+                    this.initialLoadFinished = true;
+                    this._lastValue = this.getValue();
+    
+                    if (this.getGrowToFitContent()) {
+                        var images = Ext.get(this.closureEditor.field).select('img');
+                        if (images.elements.length > 0) {
+                            Rally.util.Ui.executeCallbackWhenImagesLoaded(images, this._growToFitContent, this);
+                        }
+                        else {
+                            this._growToFitContent();
+                        }
+                    }
+    
+                    this._ieClosureFocusWorkaround();
+                    this.fireEvent('load', this);
+
+                    //This is the only added line
+                    Ext.defer(function() { this.fireEvent('afterload', this); }, 1, this);
+                }, this);
+            },
         }];
 
     },
